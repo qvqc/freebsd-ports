59787907be5c08e0751299e562336676663c8cda
qvqc <ridethe@candy.surf>
2020-05-09 01:28:29 -0400

    touchup patch

--- net-p2p/wownero-cli/files/patch-src_common_stack__trace.cpp
+++ net-p2p/wownero-cli/files/patch-src_common_stack__trace.cpp
@@ -1,9 +1,9 @@
---- src/common/stack_trace.cpp.orig     2020-05-09 01:44:29 UTC
+--- src/common/stack_trace.cpp.orig     2020-05-09 05:18:25 UTC
 +++ src/common/stack_trace.cpp
 @@ -26,13 +26,14 @@
  // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
  // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
 -#if !defined __GNUC__ || defined __MINGW32__ || defined __MINGW64__ || defined __ANDROID__
 +#if !defined __GNUC__ || defined __MINGW32__ || defined __MINGW64__ || defined __ANDROID__ || defined __FreeBSD__
  #define USE_UNWIND
@@ -11,7 +11,7 @@
  #define ELPP_FEATURE_CRASH_LOG 1
  #endif
  #include "easylogging++/easylogging++.h"
- 
+
 +#include <iomanip>
  #include <stdexcept>
  #ifdef USE_UNWIND
